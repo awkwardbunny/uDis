690ab86a9f365a744487565ee7c70448fefde880
Brian Hong <brian@hongs.me>
2022-08-05 06:48:48 -0400

    Working disassembler
    TODO: Decompiler

---
+++ main.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+import argparse
+import datetime
+import logging
+import os
+import subprocess
+from dataclasses import dataclass
+from typing import List, Dict, Any
+
+import coloredlogs
+
+log = logging.getLogger(__name__)
+coloredlogs.install(level="DEBUG")
+
+
+def get_git_version() -> str:
+    status = subprocess.run("git describe --always".split(), capture_output=True)
+    return status.stdout.decode().strip()
+
+
+VERSION = get_git_version()
+
+
+@dataclass
+class CodeBlock:
+    name: str
+    descriptor: str
+    args: List[str]
+    bytecode: List[str]
+
+    def set_args(self, args: List[str]):
+        self.args = args
+
+    def add_bytecode(self, b: str):
+        self.bytecode.append(b)
+
+
+def disassemble(filename: str) -> Dict[str, CodeBlock]:
+    module_name = filename[:-4]
+    log.info(f"Disassembling '{module_name}'...")
+
+    # Get disassembly
+    status = subprocess.run(f"./micropython/micropython -v -v -v -v -m {module_name}".split(), capture_output=True)
+    output = status.stdout.decode().split("\n")
+
+    " Errors can usually be ignored since we are not actually trying to run the files "
+    " Uncomment for debug "
+    # for e in status.stderr.decode().split("\n"):
+    #     log.debug(e)
+
+    code_blocks: Dict[str, CodeBlock] = dict()
+    current_block: CodeBlock = None
+    for line in output:
+        if len(line.strip()) == 0:
+            continue
+        if line[0] == ' ' or line[0] == '(' or line.startswith("Raw bytecode"):
+            continue
+
+        line = line.strip()
+        if line.startswith("File"):
+            if current_block is not None:
+                code_blocks[current_block.name] = current_block
+
+            block_name = line.split("'")[1]
+            block_desc = line.split()[6][:-1]
+            current_block = CodeBlock(block_name, block_desc, [], [])
+            log.info(f"* Code block '{block_name}' ({block_desc})")
+            continue
+
+        if line.startswith("arg names:"):
+            args = line.split()[2:]
+            current_block.set_args(args)
+            continue
+        current_block.add_bytecode(line)
+    if current_block is not None:
+        code_blocks[current_block.name] = current_block
+    return code_blocks
+
+
+def write_dis_to_file(cb: Dict[str, CodeBlock], filepath: str):
+    log.info(f"Writing disassembly to '{filepath}'")
+    with open(filepath, 'w') as out_f:
+        for b in cb.values():
+            out_f.write(f"# def {b.name}({','.join(b.args)}) #{b.descriptor}\n")
+            out_f.write(f"{b.name}:\n")
+            for bc in b.bytecode:
+                out_f.write(f"  {bc}\n")
+            out_f.write("\n")
+
+
+def decompile_code_block(name: str,
+                         blocks: Dict[str, CodeBlock],
+                         buf: List[str],
+                         stack: List[Any],
+                         tab_width: int = 4,
+                         depth: int = 0):
+    tab = " " * tab_width
+    current_block = blocks[name]
+
+    log.debug("TODO")
+
+    # test
+    buf.append(f"{tab * depth}{{")
+    if depth < 5:
+        decompile_code_block(name, blocks, buf, stack, tab_width, depth + 1)
+    buf.append(f"{tab * depth}}}")
+
+    # for bc in current_block.bytecode:
+    #     log.debug(bc)
+    #     match bc.split():
+    #         case [offset, "LOAD_CONST_SMALL_INT" val]:
+    #
+    #         case x:
+
+
+def decompile(module_name: str, dis: Dict[str, CodeBlock], fn_dec: str):
+    log.info(f"Decompiling '{module_name}'...")
+    timestamp = datetime.datetime.now()
+
+    buf: List[str] = []
+    stack: List[Any] = []
+    decompile_code_block('<module>', dis, buf, stack)
+
+    with open(fn_dec, 'w') as out_f:
+        out_f.write(f"####################################\n")
+        out_f.write(f"## Decompiled with uDis ({VERSION}) ##\n")
+        out_f.write(f"## At: {timestamp} ##\n")
+        out_f.write(f"####################################\n\n")
+        for line in buf:
+            out_f.write(f"{line}\n")
+
+
+def main(args: List[str] = None):
+    log.info(f" *** uDis MicroPython Decompiler ({VERSION}) *** ")
+    parser = argparse.ArgumentParser(description='Decompile MicroPython modules')
+    parser.add_argument('input_dir', type=str, help='Input directory containing mpy files')
+    parser.add_argument('output_dir', type=str, help='Output directory')
+    args = parser.parse_args(args)
+
+    for in_f in os.listdir(os.fsencode(args.input_dir)):
+        fn = os.fsdecode(in_f)
+        if fn[-4:] != ".mpy":
+            continue
+
+        module_name = f"{fn[:-4]}"
+        fn_mpy = f"{args.input_dir}/{fn}"
+        fn_asm = f"{args.output_dir}/{module_name}.s"
+        fn_dec = f"{args.output_dir}/{module_name}.py"
+
+        dis = disassemble(fn_mpy)
+        write_dis_to_file(dis, fn_asm)
+        decompile(module_name, dis, fn_dec)
+    log.info("Done.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ micropython/README.md
@@ -0,0 +1,3 @@
+This directory contains files from the [MicroPython](https://github.com/micropython/micropython) project. The [LICENSE] file, fetched from the MicroPython repo, applies to the files contained in this directory.
+
+The `micropython` binary is the [Unix port](https://docs.micropython.org/en/latest/develop/gettingstarted.html#building-the-unix-port-of-micropython) built from `v1.18` version of the source. The Python files in `py/` and `tools/` were fetched from the same version.
