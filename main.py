7aef0e178140f31b066b0e0aee07eedafa1637a4
Brian Hong <brian@bhong.dev>
2022-08-15 07:29:22 -0400

    Refractor

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import logging
 import os
 import subprocess
 import traceback
-from dataclasses import dataclass
-from typing import List, Dict, Any, Callable, Tuple
+from dataclasses import dataclass, field
+from typing import List, Dict, Any, Callable, Tuple, Union
 from pprint import pprint
 
+from lark import Lark
 import coloredlogs
 
 log = logging.getLogger(__name__)
@@ -24,148 +25,183 @@ VERSION = get_git_version()
 
 
 @dataclass
+class Bytecode:
+    offset: int
+    opcode: str
+    operands: str
+    lineno: Union[int | None]
+    attr: Dict[Any, Any] = field(default_factory=dict)
+
+    def get_opcode(self) -> str:
+        return self.opcode
+
+    def line_str(self) -> str:
+        return f"   # line {self.lineno}" if self.lineno is not None else ""
+
+    def __str__(self) -> str:
+        return f"{self.offset} {self.opcode} {self.operands}{self.line_str()}"
+
+
+@dataclass
 class CodeBlock:
     name: str
-    source_file: str
-    descriptor: str
+    source: str
+    desc: str
     args: List[str]
-    bytecode: List[Tuple[int, str, str]]
-    line_info: Dict[int, int]
+    code: Dict[int, Bytecode]
 
     def set_args(self, args: List[str]):
         self.args = args
 
-    def add_bytecode(self, b: Tuple[int, str, str]):
-        self.bytecode.append(b)
-
-    def add_line_info(self, bc: int, line: int):
-        self.line_info[bc] = line
-
-
-def disassemble(filename: str) -> Dict[str, CodeBlock]:
-    module_name = filename[:-4]
-    log.info(f"Disassembling '{module_name}'...")
-
-    # Get disassembly
-    status = subprocess.run(f"./micropython/micropython -v -v -v -v -m {module_name}".split(), capture_output=True)
-    output = status.stdout.decode().split("\n")
-
-    " Errors can usually be ignored since we are not actually trying to run the files "
-    " Uncomment for debug "
-    # for e in status.stderr.decode().split("\n"):
-    #     log.debug(e)
-
-    code_blocks: Dict[str, CodeBlock] = dict()
-    current_block: CodeBlock = None
-    for line in output:
-        if line.startswith("mem"):
-            break  # end of disassmbly
-
-        if len(line.strip()) == 0:
-            continue
-        if line[0] == '(' or line.startswith("Raw bytecode"):
-            continue
-
-        if line.startswith("  "):
-            parts = line.strip().split()
-            bc = int(parts[0].split('=')[1])
-            line = int(parts[1].split('=')[1])
-            current_block.add_line_info(bc, line)
-            continue
-
-        if line[0] == ' ':
-            continue
-
-        line = line.strip()
-        if line.startswith("File"):
-            if current_block is not None:
-                code_blocks[current_block.descriptor] = current_block
-
-            source_name = line[5:].split(',')[0]
-            block_name = line.split("'")[1]
-            block_desc = line.split()[6][:-1]
-            current_block = CodeBlock(block_name, source_name, block_desc, [], [], {})
-            log.info(f"* Code block '{block_name}' ({block_desc})")
-            continue
-
-        if line.startswith("arg names:"):
-            args = line.split()[2:]
-            current_block.set_args(args)
-            continue
-
-        parts = line.split()
-
-        # HACKY: handle edge case where a newline inside const was printed
-        try:
-            num = int(parts[0])
-        except ValueError:
-            last = current_block.bytecode[-1]
-            offset, op, args = last
-            args += "\n" + line
-            current_block.bytecode[-1] = (offset, op, args)
-            continue
-
-        instr = parts[1]
-        operands = ' '.join(parts[2:])
-        current_block.add_bytecode((num, instr, operands))
-    if current_block is not None:
-        code_blocks[current_block.descriptor] = current_block
-    return code_blocks
-
-
-def write_dis_to_file(cb: Dict[str, CodeBlock], filepath: str):
-    log.info(f"Writing disassembly to '{filepath}'")
-    with open(filepath, 'w') as out_f:
-        for b in cb.values():
-            out_f.write(f"# def {b.name}({','.join(b.args)}) #{b.descriptor}\n")
-            out_f.write(f"{b.name}:\n")
-            for bc in b.bytecode:
-                out_f.write(f"  {bc[1]} {bc[2]}\n")
-            out_f.write("\n")
+    def add_bytecode(self, b: Bytecode):
+        self.code[b.offset] = b
 
 
 class Buffer:
-    buf: List[str] = list()
+    buf: List[str]
 
-    def newline(self):
-        self.buf.append("")
+    def __init__(self):
+        self.buf = []
 
-    def append(self, s: str):
-        log.error(f"::EMIT {s}")
+    def print(self, s: str = ""):
+        self.write(s+"\n")
+
+    def write(self, s: str, print: bool = False):
+        if print:
+            log.error(f"::EMIT {s}")
         self.buf.append(s)
 
-
-class Stack:
-    buf: List[Any] = list()
-
-    def push(self, v: Any):
-        self.buf.insert(0, v)
-
-    def pop(self) -> Any:
-        return self.buf.pop(0)
-
     def dump(self) -> str:
-        return f"{self.buf}"
+        return ''.join(self.buf)
 
-    def peek(self, i: int = 0) -> Any:
-        return self.buf[i]
+class uDecompiler:
+    filename: str
+    module_name: str
+    parser: Lark
 
+    tab: str = " " * 4
+    blocks: Dict[str, CodeBlock] = None
 
-def decompile(module_name: str, dis: Dict[str, CodeBlock], fn_dec: str):
-    log.info(f"Decompiling '{module_name}'...")
-    timestamp = datetime.datetime.now()
+    def __init__(self, mpy_fn: str, module_name: str):
+        self.filename = mpy_fn
+        self.module_name = module_name
+        self.parser = None  # TODO
 
-    from Decompiler import uDecompiler
-    dec = uDecompiler(dis)
-    buf = dec.decompile()
+    def disassemble(self):
+        log.info("Disassembling")
 
-    with open(fn_dec, 'w') as out_f:
-        out_f.write(f"####################################\n")
-        out_f.write(f"## Decompiled with uDis ({VERSION}) ##\n")
-        out_f.write(f"## At: {timestamp} ##\n")
-        out_f.write(f"####################################\n\n")
-        out_f.write(buf)
-        out_f.write("\n")
+        # Get disassembly
+        status = subprocess.run(f"./micropython/micropython -v -v -v -v -m {self.filename}".split(), capture_output=True)
+        output = status.stdout.decode().split("\n")
+
+        " Errors can usually be ignored since we are not actually trying to run the files "
+        " Uncomment for debug "
+        # for e in status.stderr.decode().split("\n"):
+        #     log.debug(e)
+
+        self.blocks: Dict[str, CodeBlock] = {}
+        lineinfo: Dict[int, int] = {}
+
+        current_block: CodeBlock = None
+        for line in output:
+            if line.startswith("mem"):
+                break  # end of disassmbly
+            if len(line.strip()) == 0:
+                continue
+            if line[0] == '(' or line.startswith("Raw bytecode"):
+                continue
+
+            # Source line information
+            if line.startswith("  "):
+                parts = line.strip().split()
+                bc = int(parts[0].split('=')[1])  # bc=x
+                line = int(parts[1].split('=')[1])  # line=x
+                lineinfo[bc] = line
+                continue
+
+            if line[0] == ' ':
+                continue
+
+            line = line.strip()
+            if line.startswith("File"):
+                if current_block is not None:
+                    self.blocks[current_block.desc] = current_block
+
+                source_name = line[5:].split(',')[0]
+                block_name = line.split("'")[1]
+                block_desc = line.split()[6][:-1]
+                current_block = CodeBlock(block_name, source_name, block_desc, [], {})
+                log.info(f"* Code block '{block_name}' ({block_desc})")
+                continue
+
+            if line.startswith("arg names:"):
+                args = line.split()[2:]
+                current_block.set_args(args)
+                continue
+
+            parts = line.split()
+
+            # HACKY: handle edge case where a newline inside const was printed
+            try:
+                num = int(parts[0])
+            except ValueError:
+                last = current_block.code[-1]
+                last.operands += "\n" + line
+                continue
+
+            instr = parts[1]
+            operands = ' '.join(parts[2:])
+            lineno = lineinfo[num] if num in lineinfo.keys() else None
+            current_block.add_bytecode(Bytecode(num, instr, operands, lineno))
+        if current_block is not None:
+            self.blocks[current_block.desc] = current_block
+
+    def get_disassembly(self) -> str:
+        if self.blocks is None:
+            self.disassemble()
+
+        timestamp = datetime.datetime.now()
+
+        buf = Buffer()
+        buf.print(f"######################################")
+        buf.print(f"## Disassembled with uDis ({VERSION}) ##")
+        buf.print(f"## At: {timestamp}   ##")
+        buf.print(f"######################################\n")
+
+        for block in self.blocks.values():
+            buf.print(f"## Source: {block.source}")
+            buf.print(f"## Name:   {block.name}")
+            buf.print(f"## Args:   {block.args}")
+
+            for bc in block.code.values():
+                if bc.lineno is not None:
+                    buf.print()
+                if bc.opcode == "MAKE_FUNCTION":
+                    func_name = self.blocks[bc.operands].name
+                    buf.print(f"{bc.opcode} {bc.operands}({func_name}){bc.line_str()}")
+                else:
+                    buf.print(f"{bc.opcode} {bc.operands}{bc.line_str()}")
+            buf.print()
+        buf.print()
+        return buf.dump()
+
+    def decompile(self) -> str:
+        if self.blocks is None:
+            self.disassemble()
+
+        log.info("Decompiling")
+        timestamp = datetime.datetime.now()
+
+        buf = Buffer()
+        buf.print(f"####################################")
+        buf.print(f"## Decompiled with uDis ({VERSION}) ##")
+        buf.print(f"## At: {timestamp} ##")
+        buf.print(f"####################################\n")
+
+        # TODO
+
+        buf.print()
+        return buf.dump()
 
 
 def main(args: List[str] = None):
@@ -185,9 +221,14 @@ def main(args: List[str] = None):
         fn_asm = f"{args.output_dir}/{module_name}.s"
         fn_dec = f"{args.output_dir}/{module_name}.py"
 
-        dis = disassemble(fn_mpy)
-        write_dis_to_file(dis, fn_asm)
-        decompile(module_name, dis, fn_dec)
+        d = uDecompiler(fn_mpy, module_name)
+        dis = d.get_disassembly()
+        with open(fn_asm, 'w') as f_asm:
+            f_asm.write(dis)
+
+        dec = d.decompile()
+        with open(fn_dec, 'w') as f_dec:
+            f_dec.write(dec)
     log.info("Done.")
 
 
